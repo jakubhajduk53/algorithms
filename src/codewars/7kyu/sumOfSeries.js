a3924037a49964156b9fc4548343dbf1e66399c3
Jakub Hajduk <jakubhajduk53@gmail.com>
2025-05-11 14:29:19 +0200

    feat: add sum of series

---
+++ src/codewars/7kyu/sumOfSeries.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/555eded1ad94b00403000071
+
+function SeriesSum(n) {
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    sum += 1 / (1 + i * 3);
+  }
+  return sum.toFixed(2);
+}
