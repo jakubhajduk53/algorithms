a10d4be487ba9665b35a6c0af44437faac6fad4c
Jakub Hajduk <jakubhajduk53@gmail.com>
2025-05-12 14:04:38 +0200

    feat: add summation

---
+++ src/codewars/8kyu/summation.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/55d24f55d7dd296eb9000030
+
+var summation = function (num) {
+  let counter = 0;
+  for (let i = 0; i <= num; i++) {
+    counter += i;
+  }
+  return counter;
+};
