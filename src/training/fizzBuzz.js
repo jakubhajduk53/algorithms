4db872c229d5f487ae233221acb946a47b7ed96e
Jakub Hajduk <jakubhajduk53@gmail.com>
2025-07-04 11:13:55 +0200

    feat: add fizz buzz n

--- src/training/fizzBuzz.js
+++ src/training/fizzBuzz.js
@@ -1,6 +1,16 @@
-const fizzBuzz = (num) => {
+const fizzBuzz = (i) => {
   let result = "";
   if (num % 3 === 0) result += "Fizz";
   if (num % 5 === 0) result += "Buzz";
+  return result;
+};
+const fizzBuzzN = (n) => {
+  let result = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) result.push("FizzBuzz");
+    else if (i % 3 === 0) result.push("Fizz");
+    else if (i % 5 === 0) result.push("Buzz");
+    else result.push(i);
+  }
   return result;
 };
